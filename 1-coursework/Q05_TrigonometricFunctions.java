5e3dfa82ec19e09a9c5359404b6742c4a3f2dbf8
Kaaafiaal <ravenraiyan@gmail.com>
2026-07-01 15:45:02 +0600

    week-01 assignment code practices

---
+++ 1-coursework/Q05_TrigonometricFunctions.java
@@ -0,0 +1,14 @@
+//Question-05
+//Math.sqrt usage
+public class Q05_TrigonometricFunctions {
+    public static void main(String [] args){
+        double a = 4.5;
+        double b = 9.5;
+        double c = Math.sqrt((a*a)+(b*b));
+        double sinA = a/c;
+        double cosA= b/c ;
+        double sinB = b/c;
+        double cosB = a/c;
+        System.out.print("sin(A):"+sinA+" ,cos(A):"+cosA+" ,sin(B):"+sinB+" ,cos(b):"+cosB);
+    }
+}
