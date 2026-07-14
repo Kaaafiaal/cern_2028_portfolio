f9ecf6e9f6d7c28278dc6d688aa7b24965bf6266
Kaaafiaal <ravenraiyan@gmail.com>
2026-07-14 23:48:43 +0600

    Week-02
    Getting more familiarized into conditionals

---
+++ 1-coursework/DivisibilityProblem.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class DivisibilityProblem{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if(num%5==0 && num%7==0){
+            System.out.println("Invalid: Divisible by both");
+        }
+        else if(num%5==0){
+            System.out.println("Divisible by 5 Only");
+        }
+        else if (num%7==0){
+            System.out.println("Divisible by 7 Only");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
